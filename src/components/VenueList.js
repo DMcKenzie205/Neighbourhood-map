bbe79557a39860e979fc8db17af1de0b3ec23442
Daniel McKenzie <d.mckenzie205@gmail.com>
2018-12-13 21:02:30 -0200

    a list of venues

---
+++ src/components/VenueList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import propTypes from 'prop-types'
+
+import VenueItem from './VenueItem'
+
+import '../styles/VenueList.css'
+
+const VenueList = ({ venues, onClick, onHover }) => {
+  return (
+    <div className='menu VenueList'>
+      <p className="menu-label">Venues</p>
+      <div className="VenueList__inner">
+        <ul className='menu-list'>
+          { venues.map(venue => (
+            <VenueItem
+              key={venue.id}
+              onClick={onClick}
+              onHover={onHover}
+              {...venue}
+            />)
+          ) }
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+VenueList.defaultProps = {
+  venues: []
+}
+VenueList.propTypes = {
+  onClick: propTypes.func,
+  venues: propTypes.array.isRequired
+}
+
+export default VenueList
