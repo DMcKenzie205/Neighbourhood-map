c5257e4148c72cb502eca34e3f23df343696747e
Daniel McKenzie <d.mckenzie205@gmail.com>
2018-12-13 18:48:39 -0200

    fetch foursquare API code added

--- src/App.js
+++ src/App.js
@@ -21,7 +21,39 @@ class App extends Component {
       v: '20181122'
     }
 
+    this.apiUrl = `https://api.foursquare.com/v2/`
+  }
 
+  componentDidMount () {
+    this.fetchAllVenues()
+  }
+
+  formatParams(params) {
+    return Object.keys(params)
+            .map(key => `${key}=${params[key]}`)
+            .join('&')
+  }
+
+  fetchAllVenues() {
+    const params = {
+      ...this.params,
+      near: 'newcastle-upon-tyne',
+      radius: 750,
+      limit: 50,
+      categories: '4d4b7104d754a06370d81259'
+    }
+    const endpointUrl = `${this.apiUrl}venues/search?${this.formatParams(params)}`
+
+    fetch(endpointUrl)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          venues: data.response.venues,
+          isLoading: false,
+        })
+      )
+      .catch(error => this.setState({ error, isLoading: false }))
+  }
 
   render() {
       const { venues, activeVenue } = this.state
